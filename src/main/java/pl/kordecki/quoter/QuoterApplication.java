7e5e3ee068872f5d63ace5f6514db520f75e78ac
Kamil Kordecki <kkordecky@gmail.com>
2022-10-14 13:08:51 +0200

    Install Maven and Spring

---
+++ src/main/java/pl/kordecki/quoter/QuoterApplication.java
@@ -0,0 +1,13 @@
+package pl.kordecki.quoter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kordecki/quoter/QuoterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kordecki.quoter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
