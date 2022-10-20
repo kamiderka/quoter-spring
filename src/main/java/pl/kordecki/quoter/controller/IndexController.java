430b786fa1a935f22059974ea09c05b2ae29e17a
Kamil Kordecki <kkordecky@gmail.com>
2022-10-20 17:22:51 +0200

    Add PostMapping method to IndexController

--- src/main/java/pl/kordecki/quoter/controller/IndexController.java
+++ src/main/java/pl/kordecki/quoter/controller/IndexController.java
@@ -1,8 +1,11 @@
 package pl.kordecki.quoter.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.kordecki.quoter.model.Quote;
 
 @Controller
@@ -12,6 +15,15 @@ public class IndexController {
     public String index(Model model){
 
         model.addAttribute("quoteForm", new Quote());
+        return "index";
+    }
+
+    @PostMapping("/addQuote")
+    public String addQuote(@ModelAttribute Quote quote, Model model){
+
+        System.out.println(quote);
+        model.addAttribute("quoteForm", new Quote());
+
         return "index";
     }
 }
