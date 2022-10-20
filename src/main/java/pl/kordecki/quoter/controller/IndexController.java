562bff905b6e5f15ff519ae546b0686c9492735d
Kamil Kordecki <kkordecky@gmail.com>
2022-10-20 17:07:34 +0200

    Add form of uploading Quote in index

--- src/main/java/pl/kordecki/quoter/controller/IndexController.java
+++ src/main/java/pl/kordecki/quoter/controller/IndexController.java
@@ -1,13 +1,17 @@
 package pl.kordecki.quoter.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.kordecki.quoter.model.Quote;
 
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+
+        model.addAttribute("quoteForm", new Quote());
         return "index";
     }
 }
